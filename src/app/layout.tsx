14559a59146251424f5b3d6d68af0706cae80848
b0jun <bjkim0228@naver.com>
2024-08-25 16:12:19 +0900

    수정: 홈 및 tailwind, prettier 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { Inter, Noto_Sans, Noto_Sans_KR } from 'next/font/google';
+import { Noto_Sans, Noto_Sans_KR } from 'next/font/google';
 
 import type { Metadata } from 'next';
 import './globals.css';
@@ -7,18 +7,18 @@ const notoSansKr = Noto_Sans_KR({ subsets: ['latin'], weight: ['400', '700', '90
 const notoSans = Noto_Sans({ subsets: ['latin'], weight: ['400', '700', '900'] });
 
 export const metadata: Metadata = {
-	title: '돌잔치',
-	description: '돌잔치 랭킹현황표',
+  title: '돌잔치',
+  description: '돌잔치 랭킹현황표',
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ko">
-			<body className={`${notoSansKr.className} ${notoSans.className}`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="ko">
+      <body className={`${notoSansKr.className} ${notoSans.className}`}>{children}</body>
+    </html>
+  );
 }
