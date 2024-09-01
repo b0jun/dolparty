310e3716a4e59de420972242768cb0e2c59e8c5b
b0jun <bjkim0228@naver.com>
2024-09-01 16:06:04 +0900

    검색 수정

--- src/app/judge/contestant/[id]/page.tsx
+++ src/app/judge/contestant/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { SearchIcon } from '@/svg';
 import Problems from './Problems';
 
 const ContestantDetail = () => {
@@ -5,15 +6,20 @@ const ContestantDetail = () => {
     <main className="flex min-h-full flex-col">
       <div className="flex min-w-[768px] flex-1 bg-backdrop bg-cover bg-center bg-no-repeat">
         <div className="mx-auto w-[1024px] min-w-[768px] bg-white/50 px-[16px] py-[24px]">
-          <div className="mb-[24px] flex items-center gap-4">
+          <div className="mb-[24px] flex items-center justify-end gap-4">
             <label htmlFor="contestantNumber" className="font-medium">
               선수 번호
             </label>
-            <input
-              id="contestantNumber"
-              placeholder="번호를 입력해주세요."
-              className="flex-1 rounded-lg border border-black/10 bg-transparent p-4 outline-none"
-            />
+            <div className="flex overflow-hidden rounded-lg border border-black/10 bg-white/30">
+              <input
+                id="contestantNumber"
+                placeholder="번호를 입력해주세요."
+                className="h-[50px] w-[200px] border-none bg-transparent p-4 text-[22px] outline-none placeholder:text-[18px]"
+              />
+              <button className="flex w-[60px] items-center justify-center bg-[#3F4E4F]">
+                <SearchIcon fill="white" width={24} height={24} />
+              </button>
+            </div>
           </div>
           <div className="sticky top-0 z-10 mb-[24px] grid grid-cols-3 gap-4 rounded-lg border border-black/10 bg-[#f4f4f4] p-4">
             <div className="flex flex-col">
--- src/svg/index.ts
+++ src/svg/index.ts
@@ -1,5 +1,6 @@
 import MinusIcon from './minus.svg';
 import PlusIcon from './plus.svg';
 import TshirtIcon from './tshirt.svg';
+import SearchIcon from './search.svg';
 
-export { MinusIcon, PlusIcon, TshirtIcon };
+export { MinusIcon, PlusIcon, TshirtIcon, SearchIcon };
